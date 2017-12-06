410bf42443b32b1eeba7809b90029a06171f09d7
Francisco <franciscosegarra@hotmail.com>
2017-12-06 16:00:03 -0300

    comela

--- model/ModelServicios.php
+++ model/ModelServicios.php
@@ -29,9 +29,37 @@ class ModelServicios{
     return ($this->consulta($query));
   }
 
+  // public function addCompra($productos,$idUser)
+  // {
+  //
+  //   $model = new PDOConnection;
+  //   $connection = $model->getConnection();
+  //   $connection->beginTransaction();
+  //   $queryDemograficos = "INSERT INTO compra (idCompra,idUsuario,fechaInicio,fechaFin,totalCosto) VALUES (NULL,'$idUser','$productos[1]['desde']','$productos[1]['hasta']','$productos[1]['total']')";
+  //   $stmt = $connection->prepare($queryDemograficos);
+  //   $stmt->execute();
+  //   $ultimos_datos = $connection->lastInsertId();
+  //
+  //   foreach ($productos as $each){
+  //   if ($each['total'] == NULL) {
+  //   $query = "INSERT INTO compraTieneServicio (idCompra,idServicio) VALUES ('$ultimos_datos','$each['indice']')";
+  //   $stmt2 = $connection->prepare($query);
+  //   $stmt2->execute();
+  //     }
+  //   }
+  //   $connection->commit();
+  // }
+
   public function getServicio($idServicio)
   {
     $query = "SELECT * FROM servicio WHERE idServicio='$idServicio'";
     return ($this->consulta($query)[0]);
   }
+
+  public function getHabitaciones($idServicio)
+  {
+    $query = "SELECT nombre FROM Habitacion INNER JOIN servicioTieneHabitacion ON Habitacion.idTipoHabitacion = servicioTieneHabitacion.idTipoHabitacion WHERE idServicio='$idServicio'";
+    return ($this->consulta($query));
+  }
+
 }
